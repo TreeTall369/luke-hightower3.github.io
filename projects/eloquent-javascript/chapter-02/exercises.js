c4e4ebb0bb767fd6b18b592bce8eb6d77bc71f3a
Luke Hightower <lukehightower11@gmail.com>
2023-01-12 20:38:39 +0000

    jan12.2

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -11,7 +11,18 @@ function triangles() {
 ////////////////////////////////////////////////////////////////////////////////
 
 function fizzBuzz() {
-  
+
+  for (let index = 1; index < 16; index++){
+    if (index % 15 === 0){
+      console.log('fizzbuzz')
+    } else if (index % 5 === 0){
+      console.log('buzz')
+    } else if (index % 3 === 0){
+      console.log('fizz')
+    } else {
+      console.log(index)
+    }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
