8c6cb97bd010bb8aa20b6de22a829e3580a80a02
Luke Hightower <lukehightower11@gmail.com>
2023-02-02 03:07:07 +0000

    feb1.666

--- lodown/index.js
+++ lodown/index.js
@@ -35,3 +35,195 @@ function identity(value){
 };
 
 module.exports.identity = identity;
+
+/**
+ * typeOf
+ * 
+ *  @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ */
+function typeOf(){
+
+}
+module.exports.
+/**
+ * first 
+ *  @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function first(){
+
+}
+module.exports.
+/**
+ * last 
+ *  @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function last(){
+
+}
+module.exports.
+/**
+ * indexOf
+ *  @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function indexOf(){
+
+}
+module.exports.
+/**
+ * contains
+ * @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ */
+function contains(){
+
+}
+module.exports.
+/**
+ * each
+ * @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function each(){
+
+}
+module.exports.
+/**
+ * unique
+ * @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function unique(){
+
+}
+module.exports.
+/**
+ * filter
+ * @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function filter() {
+    
+}
+module.exports.
+/**
+ * rejet
+ * @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function reject(){
+
+}
+module.exports.
+/**
+ * partition
+ * @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function partition(){
+
+}
+module.exports.
+/**
+ * map
+ * @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function map(){
+
+}
+module.exports.
+/**
+ * pluck
+ * @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function pluck(){
+
+}
+module.exports.
+/**
+ * every
+ * @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function every(){
+
+}
+module.exports.
+/**
+ * some
+ * @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function some(){
+
+}
+module.exports.
+/**
+ * reduce
+ * @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function reduce(){
+
+}
+module.exports.
+/**
+ * extend
+ * @param { any:value }: function takes in any value
+ * @return { any:value}: function returns input value unchanged
+ * 
+ * 
+ * 
+ */
+function extend(){
+    
+}
+module.exports.
