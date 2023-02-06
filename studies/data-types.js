ec7d4da6e4471b4239cc40dbefb552834a3b267f
Luke Hightower <lukehightower11@gmail.com>
2023-02-06 13:43:26 +0000

    feb7.1

--- studies/data-types.js
+++ studies/data-types.js
@@ -2,36 +2,147 @@
  *          DATA TYPES: 
  *          
  *    0.    The way we use the word data can be tricky. To be precise about the word referring to information
- *          data are collections of datum. Some data is a collection of these collections. This could be
- *          considered entirely useless information though it would still be plural. 
- */
-
-
-
-
- /*    1.    Types of data, and the nuances of their differences is very important! To begin, a list of 
+ *          data are collections of datum. Some data is a collection of these collections. Types of data, and 
+ *          the nuances of their differences is very important! To begin, a list of 
  *          data types: 
- *                  number 99 - numbers function mathematical ie decimal point and aritmetic operators, primitive
- *                  boolean true or false - true is greater than false, true is 1 and false is 0? primitive
- *                  string 'anything inside the quotes' - characters have number value... primitive data
- *                  array  ['information'] - complex data that holds other data, can be empty
- *                  object  { key:value } - complex data hold info in unorderlist by key:value pairs, use commas
- *                  function   function(){} - can take input, can return output, if called fx's are expressions
- *                  undefined  is undefined - signaling lack of 'pointing' to anything
- *                  null  is an object... 
- *                  Nan         
+ * 
+ *                  numbers 
+ *                  boolean 
+ *                  string 
+ *                  array  
+ *                  object  
+ *                  function  
+ *                  undefined 
+ *                  null 
+ *                  NAN         
  *                  Infinity  
- *                  neg.Infinity
+ *                  -Infinity
+ * 
+ *          Primitive Datatypes are simple, ie they have single piece of datum. They are not collections
+ *          and thus will copy value. 
+ * 
+ *          Complex Datatypes are multitudinous in their being. Complex data included collections and 
+ *          functions and thus copy by reference instead of value. 
+ * 
+ *          To copy by value we can use the spread opertator which creates a shallow copy;
+ *          Copy by reference is when a collection = a and then we say that now x = a and thus 
+ *          by reference equals the collection. This refers to the data and does not create a shallow
+ *          copy.
+ */          
+
+        let abc = {1: 'a', 2: 'b', 3: 'c'}
+        let xyz = {...abc}
+        console.log(xyz) // this is copy by value and will log the new object
+
+        let lmo = abc;
+        console.log(lmo) // this is copy by reference and does not create a shallow copy
+
+ /*    1.  Numbers
+ *                  
+ * 
+ * 
+ * 
  * 
  */
 
  
 
 
- /*    2.    What are primitive data types? Complex?
- *          What is copy by value? When does it matter?
- *          What is copy by reference? How it differs from copy by value? 
- *          Dont forget console.log()
+ /**   2.  Boolean
+ *    
+ * 
+ * 
+ *     
+ * 
+ */
+
+
+
+
+/**    3.  String
+ *    
+ * 
+ * 
+ *     
+ * 
+ */
+
+
+
+
+/**    4.  Array
+ *    
+ * 
+ * 
+ *     
+ * 
+ */
+
+
+
+
+
+
+/**    5.  Object
+ *    
+ * 
+ * 
+ *     
+ * 
+ */
+
+
+
+
+/**    6.  Function
+ *    
+ * 
+ * 
+ *     
+ * 
+ */
+
+
+
+
+/**     7.   Undefined
+ *    
+ * 
+ * 
+ *     
+ * 
+ */
+
+
+
+
+/**     8.   Null
+ *    
+ * 
+ * 
+ *     
+ * 
+ */
+
+
+
+/**     9.   Nan
+ *    
+ * 
+ * 
+ *     
+ * 
+ */
+
+
+
+
+/**     10.   Infinity and -Infinity
+ *    
+ * 
+ * 
+ *     
+ * 
  */
 
 
