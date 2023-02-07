8bc0861aa69b6d28bd596082e417197b534a192d
Luke Hightower <lukehightower11@gmail.com>
2023-02-07 00:33:53 +0000

    feb8

--- studies/string-manipulation.js
+++ studies/string-manipulation.js
@@ -3,33 +3,32 @@
  * 
  *            with operators, with string methods
  * 
- *     0.   Strings are a crucial part of JavaScript. Our ability to manipulate words in 
- *          any way possible will be eseential throughout website development. Spellcheck, predictive-text
- *          text upload, password chechking, etc all involve the movement of strings. Strings, like arrays
- *          have indices going from 0 - very large from left to right. We can use for loops to 
- *          take a string and loop through each of it's characters. There are critical methods to strings
- *          that allow us to find characters, splits the string up into an array and join the string from an
- *          array back into string, als strings can be upper and lower cased etc. Each letter of the alphabet 
- *          case aside is also represented as a nimber code allowing us to pass the string info in multiple
- *          ways and to compare strings in helpful way too. The + operator is always partiuclarly helpful in 
- *          concatenating strings though the + often needs ' ' around it to make legible sentences. The manipulation 
- *          of strings parallel that of arrays. Both have indexes and are accessible through bracket notation.
- *          Develops are commonly moving values between arrays and strings depending neseiccty becuse of this 
- *          simillarity. 
- * 
- *          examples
- *          list of methods
- *          console
- * 
- * 
+ *     0.   Strings are a crucial part of JavaScript. Strings, like arrays
+ *          have indices starting at zer0 large from left to right. We can use for loops to 
+ *          take a string and loop through each of it's characters. Each character on the keyboard 
+ *          is represented in unicode. There are critical methods help us to manipulate strings. Split 
+ *          and join methods are two examples. These allow us to split a string into an array and to 
+ *          conversely bring an array of strings together into one string. toLowerCase() and toUpperCase()
+ *          are two other examples of method meant for strings. The + operator is also very helpful in 
+ *          concatenating strings though the + often needs ' ' around it to make legible sentences. Also
+ *          with methods to change strings and find certain info about them there are 'template literals'
+ *          which allow a develop an easier way to approach the incorporation of strings with other 
+ *          variables.
  */
 
-/**
- *      1.
- * 
- * 
- * 
- * 
- * 
- * 
- */
+        let letters = 'abcdef';
+        let arrayOfLetters = letters.split('') // [a,b,c,d,e,f]
+        let joined = arrayOfLetters.join(''); // abcdefg
+        console.log(letters, arrayOfLetters, joined)
+
+        let person = 'lower case man'
+        let upper = person.toUpperCase()
+        console.log(upper) // LOWER CASE MAN
+
+        let firstName = 'Tony'
+        let lastName = 'Tonyson'
+        console.log(firstName + ' ' + lastName) // Tony Tonyson
+
+        let greeting = 'Hi, how are you?'
+        let sendOff = 'Well, gotta go!'
+        console.log(`Leonard! ${greeting} I'm really busy today. ${sendOff}`) // Leonard! Hi how are you? I'm really busy. Well,gotta go!
